e64aab30ca7c90f5f3f777a652da4bee2b9a47ce
krylis <cmcshann@gmail.com>
2023-09-08 11:52:30 -0700

    Removed ship class and defined player ship in main.py
    Added functionality to allow user to move player ship left/right on the screen using the arrow keys

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-from ship import Ship
+import turtle
 
-player = Ship()
+screen = turtle.Screen()
+screen.screensize(600, 800, "grey")
 
-player.screen.screensize(600, 800, "grey")
-player.screen.mainloop()
+# Create player controlled ship
+player = turtle.Turtle()
+player.shape("triangle")
+player.left(90)
+player.penup()
+player.goto(0, -350)
+
+# Methods for moving the ship
+def left():
+    player.goto(player.xcor() - 10, -350)
+
+
+def right():
+    player.goto(player.xcor() + 10, -350)
+
+
+screen.onkey(left, "Left")
+screen.onkey(right, "Right")
+screen.listen()
+
+screen.mainloop()
